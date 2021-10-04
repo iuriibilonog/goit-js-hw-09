10885c35b1a89cd84907ae25a5deedb7043e2c1f
Iurii Bilonog <iuriibilonog@gmail.com>
2021-10-04 19:27:25 +0300

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+'use strict';
 
+import { getRandomHexColor } from './functions.js';
+
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyNode = document.querySelector('body');
+let changeColor = null;
+
+startBtn.addEventListener('click', () => {
+  changeColor = setInterval(() => {
+    bodyNode.style.backgroundColor = getRandomHexColor();
+  }, 1000)
+  startBtn.disabled = true;
+})
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(changeColor);
+  startBtn.disabled = false;
+})
+
+console.log(getRandomHexColor)
